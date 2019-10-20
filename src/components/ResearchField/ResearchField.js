1a682d290c7ccbdc293fe265bbe8427e983eee29
anderson-matheus <matheus5347@gmail.com>
2019-10-20 18:33:28 -0300

    validando props do componente ResearchField

--- src/components/ResearchField/ResearchField.js
+++ src/components/ResearchField/ResearchField.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Input from './Style';
 
-const ResearchField = () => (
+const ResearchField = ({ value, onChange }) => (
   <Input>
     <p>Busque por artistas, álbuns ou músicas</p>
-    <input type="text" name="search" placeholder="Comece a escrever..." />
+    <input
+      type="text"
+      name="search"
+      value={value}
+      onChange={onChange}
+      placeholder="Comece a escrever..."
+    />
   </Input>
 );
+
+ResearchField.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
+ResearchField.defaultProps = {
+  value: '',
+};
 
 export default ResearchField;
