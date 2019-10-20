5d2bd96438d9018dc6683f87dfd37691c7f5fbe4
anderson-matheus <matheus5347@gmail.com>
2019-10-20 17:20:04 -0300

    criando arquivo de utils e instalando axios

---
+++ src/utils/Utils.js
@@ -0,0 +1,9 @@
+const getUrlParams = (search) => () => {
+  const hashes = search.slice(search.indexOf('?') + 1).split('&');
+  return hashes.reduce((params, hash) => {
+    const [key, val] = hash.split('=');
+    return Object.assign(params, { [key]: decodeURIComponent(val) });
+  }, {});
+};
+
+export default getUrlParams;
--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,6 @@ module.exports = ()  =>{
     },
     output: {
       path: path.join(__dirname, '/dist'),
-      // publicPath: '/',
       filename: 'bundle.js',
     },
     plugins: [
