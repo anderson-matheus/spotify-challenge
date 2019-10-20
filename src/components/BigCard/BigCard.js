bd144277bcec2a6469c4db56abaf2e882b4408b1
anderson-matheus <matheus5347@gmail.com>
2019-10-20 10:12:40 -0300

    criando component big card

---
+++ src/components/BigCard/BigCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Album,
+  Title,
+  Image,
+  SubTitle,
+} from './Style';
+
+const BigCard = () => (
+  <Album>
+    <Image>imagem</Image>
+    <Title>título do álbum</Title>
+    <SubTitle>subtítulo do álbum</SubTitle>
+  </Album>
+);
+
+export default BigCard;
---
+++ src/components/BigCard/Style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Album = styled.article`
+  padding: 10px 0;
+
+  &:hover {
+    background-color: ${(props) => props.theme.fourthColor};
+    opacity: 0.5;
+  }
+`;
+
+export const Image = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: transparent;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export const Title = styled.p`
+  width: 100%;
+  text-align: center;
+  margin-bottom: 5px;
+  font-size: ${(props) => props.theme.regularSize};
+  color: ${(props) => props.theme.secondaryColor};
+  background-color: transparent;
+`;
+
+export const SubTitle = styled.p`
+  width: 100%;
+  text-align: center;
+  font-size: ${(props) => props.theme.smallestSize};
+  color: ${(props) => props.theme.primaryColor};
+  background-color: transparent;
+`;
