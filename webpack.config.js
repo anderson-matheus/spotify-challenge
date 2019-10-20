26b3a49dd11ac6d38e913d9333bcd23b9868058c
anderson-matheus <matheus5347@gmail.com>
2019-10-20 13:40:58 -0300

    instalando e configurando dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,54 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
-      },
-      {
-        test: /\.(jpg|png)$/,
-        use: {
-          loader: 'url-loader',
+module.exports = ()  =>{
+  const env = dotenv.config().parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: './src/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'eslint-loader'],
         },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-    modules: ['app', 'node_modules'],
-    alias: {
-      Components: path.resolve(__dirname, './src/components/'),
+        {
+          test: /\.(jpg|png)$/,
+          use: {
+            loader: 'url-loader',
+          },
+        },
+      ],
     },
-  },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    // publicPath: '/',
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-  },
-  
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+      modules: ['app', 'node_modules'],
+      alias: {
+        Components: path.resolve(__dirname, './src/components/'),
+      },
+    },
+    output: {
+      path: path.join(__dirname, '/dist'),
+      // publicPath: '/',
+      filename: 'bundle.js',
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new webpack.DefinePlugin(envKeys),
+    ],
+    devServer: {
+      historyApiFallback: true,
+    },
+  }
 };
