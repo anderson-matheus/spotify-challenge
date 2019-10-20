c7c3b19c7dccddc9b55390305fa196e6dda7cc73
anderson-matheus <matheus5347@gmail.com>
2019-10-20 06:56:47 -0300

    aplicando styles e ajustando estrutura no app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import AlbumsContainer from './containers/Albums/AlbumsContainer';
-import GlobalStyle from './GlobalStyle';
-import { Section, Article } from './Style';
+import HomeContainer from './containers/Home/HomeContainer';
+import GlobalStyle from './styles/GlobalStyle';
+import { Section, Article, Logo } from './styles/Style';
+import logo from './assets/images/spotifylogo.png';
 
 const App = () => (
   <Section>
-    <div>Logo</div>
+    <Logo>
+      <img src={logo} alt="Spotify Challenge" with="50" height="50" />
+    </Logo>
     <Article>
       <Switch>
+        <Route path="/" exact component={HomeContainer} />
         <Route path="/albums" component={AlbumsContainer} />
       </Switch>
     </Article>
