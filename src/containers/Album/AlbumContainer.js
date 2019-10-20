52a64a8296794c5be9e49d8c46184c8f668959c4
anderson-matheus <matheus5347@gmail.com>
2019-10-20 11:21:13 -0300

    estilizando componente de álbum

--- src/containers/Album/AlbumContainer.js
+++ src/containers/Album/AlbumContainer.js
@@ -1,11 +1,23 @@
 
 import React from 'react';
 import ButtonBack from 'Components/ButtonBack/ButtonBack';
+import BigCard from 'Components/BigCard/BigCard';
+import Song from 'Components/Song/Song';
+import { Container, Button } from './Style';
+
+const songs = Array.from(Array(Math.floor(Math.random() * 100)).keys());
 
 const AlbumContainer = () => (
-  <div>
-    <ButtonBack />
-  </div>
+  <Container>
+    <Button><ButtonBack /></Button>
+    <section>
+      <BigCard />
+    </section>
+
+    <section>
+      {songs.map((index) => <Song key={index} />)}
+    </section>
+  </Container>
 );
 
 export default AlbumContainer;
---
+++ src/containers/Album/Style.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  section:first-of-type {
+    float: left;
+    width: 30%;
+  }
+
+  section:last-child {
+    float: right;
+    width: 70%;
+    padding: 0 0 0 50px;
+  }
+
+  @media only screen and (max-width: 992px) {
+    section:first-of-type {
+      width: 45%;
+    }
+
+    section:last-child {
+      width: 55%;
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    section:first-of-type {
+      width: 100%;
+      margin-bottom: 30px;
+    }
+
+    section:last-child {
+      width: 100%;
+      padding: 0;
+    }
+  }
+`;
+
+export const Button = styled.span`
+  p {
+    margin-bottom: 30px;
+  }
+`;
