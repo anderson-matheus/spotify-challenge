391f42347c819c23b065c282ac81ada71c5e933b
anderson-matheus <matheus5347@gmail.com>
2019-10-20 21:09:42 -0300

    estilizando componente regular card e rebendo props

--- src/components/RegularCard/RegularCard.js
+++ src/components/RegularCard/RegularCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 import {
   Album,
   Title,
@@ -7,16 +9,43 @@ import {
   SubTitle,
 } from './Style';
 
-const albumName = 'teste';
-
-const RegularCard = () => (
+const RegularCard = ({ data }) => (
   <Album>
-    <Link to={`/albums/${albumName}`}>
-      <Image>imagem</Image>
-      <Title>título do álbum</Title>
-      <SubTitle>subtítulo do álbum</SubTitle>
+    <Link to={`/albums/${_.get(data, 'id', '')}`}>
+      <Image>
+        <img
+          src={_.get(data, 'images[1].url', '')}
+          alt={_.get(data, 'name', '')}
+          width="150"
+          height="150"
+        />
+      </Image>
+      <Title>
+        {(
+          _.get(data, 'name', '').length <= 20
+            ? _.get(data, 'name', '') : `${_.get(data, 'name', '').substr(0, 20)}...`
+        )}
+      </Title>
+      <SubTitle>
+        {(
+          _.get(data, 'artists', []).map((artist) => artist.name).join(', ').length <= 25
+            ? _.get(data, 'artists', []).map((artist) => artist.name).join(', ')
+            : `${_.get(data, 'artists', []).map((artist) => artist.name).join(', ').substr(0, 25)}...`
+        )}
+      </SubTitle>
     </Link>
   </Album>
 );
+
+RegularCard.propTypes = {
+  data: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.objectOf,
+  ]),
+};
+
+RegularCard.defaultProps = {
+  data: '',
+};
 
 export default RegularCard;
--- src/components/RegularCard/Style.js
+++ src/components/RegularCard/Style.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 export const Album = styled.article`
+  height: 260px;
+
   a {
     float: left;
     width: 100%;
+    height: 100%;
     text-decoration: none;
     padding: 10px 0;
   }
@@ -23,16 +26,21 @@ export const Image = styled.div`
 `;
 
 export const Title = styled.p`
-  width: 100%;
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
   text-align: center;
   margin-bottom: 5px;
   font-size: ${(props) => props.theme.regularSize};
   color: ${(props) => props.theme.secondaryColor};
   background-color: transparent;
+
 `;
 
 export const SubTitle = styled.p`
-  width: 100%;
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
   text-align: center;
   font-size: ${(props) => props.theme.smallestSize};
   color: ${(props) => props.theme.primaryColor};
