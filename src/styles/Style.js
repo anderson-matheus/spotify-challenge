2f4f0e32bf0626969c828a8017f0e087209ab835
anderson-matheus <matheus5347@gmail.com>
2019-10-20 21:07:45 -0300

    removendo estilização de logo para o componente app

--- src/styles/Style.js
+++ src/styles/Style.js
@@ -9,21 +9,6 @@ export const Section = styled.section`
   }
 `;
 
-export const Logo = styled.div`
-  width: 100px;
-  
-  display: flex;
-  justify-content: center;
-
-  img {
-    margin: 20px;
-  }
-
-  @media only screen and (max-width: 768px) {
-    width: 100%;
-  }
-`;
-
 export const Article = styled.article`
   width: calc(100% - 100px);
   color: #000;
