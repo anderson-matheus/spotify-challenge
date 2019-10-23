be3b21c3f67d3c5e8c5d850997f7d0c0d27df1cc
anderson-matheus <matheus5347@gmail.com>
2019-10-23 17:14:55 -0300

    carregando informações de api no componente BigCard

--- src/components/BigCard/BigCard.js
+++ src/components/BigCard/BigCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 import {
   Album,
   Title,
@@ -6,12 +8,42 @@ import {
   SubTitle,
 } from './Style';
 
-const BigCard = () => (
+const BigCard = ({ album }) => (
   <Album>
-    <Image>imagem</Image>
-    <Title>título do álbum</Title>
-    <SubTitle>subtítulo do álbum</SubTitle>
+    <Image>
+      <img
+        src={_.get(album, 'images[0].url', '')}
+        alt={_.get(album, 'name', '')}
+        width="200"
+        height="200"
+      />
+    </Image>
+    <Title>
+      {(
+        _.get(album, 'name', '').length <= 25
+          ? _.get(album, 'name', '') : `${_.get(album, 'name', '').substr(0, 25)}...`
+      )}
+    </Title>
+    <SubTitle>
+      {(
+        _.get(album, 'artists', []).map((artist) => artist.name).join(', ').length <= 30
+          ? _.get(album, 'artists', []).map((artist) => artist.name).join(', ')
+          : `${_.get(album, 'artists', []).map((artist) => artist.name).join(', ').substr(0, 30)}...`
+      )}
+    </SubTitle>
   </Album>
 );
+
+BigCard.propTypes = {
+  album: PropTypes.shape({
+    images: PropTypes.array,
+    name: PropTypes.string,
+    artists: PropTypes.array,
+  }),
+};
+
+BigCard.defaultProps = {
+  album: '',
+};
 
 export default BigCard;
