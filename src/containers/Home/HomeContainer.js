a46e68ff8b87a1801970ddf128f2b48708525556
anderson-matheus <matheus5347@gmail.com>
2019-10-23 19:33:00 -0300

    reduzindo chamadas de api com redux

--- src/containers/Home/HomeContainer.js
+++ src/containers/Home/HomeContainer.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
+import { useSelector, useDispatch } from 'react-redux';
 import ResearchField from 'Components/ResearchField/ResearchField';
 import RegularAlbumsCardList from 'Components/RegularAlbumsCardList/RegularAlbumsCardList';
 import RegularTracksCardList from 'Components/RegularTracksCardList/RegularTracksCardList';
@@ -13,62 +14,82 @@ import {
 
 const HomeContainer = () => {
   const getSearch = search;
-  const [value, setValue] = useState('');
   const [albums, setAlbums] = useState('');
   const [tracks, setTracks] = useState('');
-  const [loading, setLoading] = useState();
+  const [callApi, setCallApi] = useState(true);
+
+  const searchValue = useSelector((state) => state.searchValue);
+  const requests = useSelector((state) => state.requests);
+  const dispatch = useDispatch();
+
   const onChange = async (event) => {
-    setValue(event.target.value);
+    const { target } = event;
+    const { value } = target;
+    dispatch({ type: 'SEARCH', search: value });
+
     if (_.get(event, 'target.value.length', 0) <= 2) {
       setAlbums('');
       setTracks('');
       return;
     }
+
+    if (callApi === false) return;
+
     await getSearch(event.target.value).then((res) => {
       setAlbums(res.data.albums);
       setTracks(res.data.tracks);
+      const { data } = res;
+      data.search = value;
+      requests.push(data);
+      dispatch({ type: 'REQUESTS', request: requests });
     }).catch((err) => {
       window.console.log(err.response);
     });
   };
 
+  useEffect(() => {
+    const isExists = requests.filter((request) => _.get(request, 'search', '') === searchValue);
+    setCallApi(_.get(isExists, 'length', 0) === 0);
+    if (_.get(isExists, 'length', 0) > 0) {
+      const data = isExists.shift();
+      setAlbums(data.albums);
+      setTracks(data.tracks);
+    }
+  }, [searchValue]);
+
   const renderAlbums = () => {
-    if (localStorage.getItem('RECENTLY_VIEWED_ALBUMS') === null && _.get(value, 'length', 0) === 0) {
+    if (localStorage.getItem('RECENTLY_VIEWED_ALBUMS') === null && _.get(searchValue, 'length', 0) === 0) {
       return <p>Nenhum álbum buscado recentemente</p>;
     }
-
     if (albums) return (<RegularAlbumsCardList albums={albums} />);
-
     return <p>Sem Resultados</p>;
   };
 
   const renderTracks = () => {
-    if (localStorage.getItem('RECENTLY_VIEWED_TRACKS') === null && _.get(value, 'length', 0) === 0) {
+    if (localStorage.getItem('RECENTLY_VIEWED_TRACKS') === null && _.get(searchValue, 'length', 0) === 0) {
       return <p>Nenhuma música buscada recentemente</p>;
     }
-
     if (tracks) return (<RegularTracksCardList tracks={tracks} />);
-
     return <p>Sem Resultados</p>;
   };
 
   return (
     <Container>
-      <ResearchField value={value} onChange={onChange} />
+      <ResearchField value={searchValue} onChange={onChange} />
 
       <Content>
         {
-          value ? (
+          searchValue ? (
             <>
               <ResultTitle>
                 Resultados encontrados para álbuns &quot;
-                {value}
+                {searchValue}
                 &quot;
               </ResultTitle>
               {renderAlbums()}
               <ResultTitle>
                 Resultados encontrados para músicas &quot;
-                {value}
+                {searchValue}
                 &quot;
               </ResultTitle>
               {renderTracks()}
