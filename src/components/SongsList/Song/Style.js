7fb7233c1f919e442e8015290f4030509cc4966a
anderson-matheus <matheus5347@gmail.com>
2019-10-23 21:11:20 -0300

    adicionando estilo na listagem das músicas na página de álbum

--- src/components/SongsList/Song/Style.js
+++ src/components/SongsList/Song/Style.js
@@ -1,24 +1,35 @@
 import styled from 'styled-components';
 
 export const Music = styled.p`
-  padding: 0 0 20px 0;
+  padding: 10px;
   width: 100%;
   display: flex;
+  background-color: transparent;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme.fourthColor};
+    opacity: 0.5;
+  }
 `;
 
 export const SongNumber = styled.span`
+  background-color: transparent;
   color: ${(props) => props.theme.primaryColor};
   font-size: ${(props) => props.theme.smallestSize};
   width: 10%;
 `;
 
 export const SongName = styled.span`
+  background-color: transparent;
   color: ${(props) => props.theme.thirdColor};
   font-size: ${(props) => props.theme.smallestSize};
   width: 70%;
+  word-wrap: break-word;
 `;
 
 export const SongTime = styled.span`
+  background-color: transparent;
   color: ${(props) => props.theme.primaryColor};
   font-size: ${(props) => props.theme.smallestSize};
   width: 15%;
